fd70d89df7ea57dd34baf233ef48f076b54b01ba
SamuelWade <samuelrcwade@gmail.com>
2023-07-21 09:55:19 -0500

    lab completed

--- lib/tree.py
+++ lib/tree.py
@@ -3,4 +3,21 @@ class Tree:
     self.root = root
 
   def get_element_by_id(self, id):
-    pass
+    nodes_to_visit = [self.root]
+    while nodes_to_visit:
+      current = nodes_to_visit.pop()
+      if current['id'] == id:
+        return current
+      nodes_to_visit = nodes_to_visit + current['children']
+    return None
+      
+      
+
+# def dfs(node):
+#   nodes_to_visit = [node]
+#   result = []
+#   while len(nodes_to_visit)>0:
+#     node = nodes_to_visit.pop(0)
+#     result.append(node['value'])
+#     nodes_to_visit = node['children'] + nodes_to_visit
+#   return result
